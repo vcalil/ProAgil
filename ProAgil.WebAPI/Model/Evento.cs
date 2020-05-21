f866026871c657237d63bb5b4db503875e06db87
v.calil <v.calil@hotmail.com>
2020-05-21 14:23:36 -0300

    Inicio do projeto

---
+++ ProAgil.WebAPI/Model/Evento.cs
@@ -0,0 +1,14 @@
+namespace ProAgil.WebAPI.Model
+{
+    public class Evento
+    {
+        public int EventoID { get; set; }
+        public string Local { get; set; }
+        public string DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int QtdePessoas { get; set; }
+        public string Lote { get; set; }
+        public string ImagemURL { get; set; }
+        
+    }
+}
